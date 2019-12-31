881af1b06a62b3f2218094a902cf6774ec4f70b8
paintdog <paintdog@users.noreply.github.com>
2019-12-31 23:15:38 +0100

    Update egn.py

--- egn.py
+++ egn.py
@@ -1,45 +1,77 @@
 import csv
 import json
-# from pprint import pprint
+from bs4 import BeautifulSoup
+import requests
 
-# Your settings
 
 year = 2020
+city = "Brüggen"
+district = "Bracht"
+street = "Agrisstrasse"
+house_number = "1"
 
-null = None 
 
-# put the json-data from your webbrowser here:
-data = {"waste_discharge":{"2020":{ __Put your Data here!__ } } }
 
-def main():
+EGN_URL = "https://www.egn-abfallkalender.de/kalender"
+EGN_ALEXA_URL = "https://www.egn-abfallkalender.de/alexa"
+
+garbage_cans = {'bio':  'braune Tonne',
+                'hml':  'graue Tonne',
+                'hml2': 'None',
+                'lvp':  'gelbe Tonne',
+                'ppk':  'blaue Tonne',
+                'wbm':  'braune Tonne'}
+
+def create_filename(parameter, year):
+    ''' creates a filename city-street-street_number '''
+    return "{} - {}{} ({}).csv".format(parameter['city'],
+                                       parameter['street'],
+                                       parameter['street_number'],
+                                       year)
+
+def create_parameter(city, district, street, house_number):
+    ''' builds the parameter list '''
+    return {'city': city,
+           'district': district,
+           'street': street,
+           'street_number': house_number}
     
-    garbage_cans = {'bio':  'braune Tonne',
-                    'hml':  'graue Tonne',
-                    'hml2': 'None',
-                    'lvp':  'gelbe Tonne',
-                    'ppk':  'blaue Tonne',
-                    'wbm':  'braune Tonne'}
-    
-    null = None
+def load_data(url, parameter):
+    ''' download data from www.egn-abfallkalender.de '''
+    session = requests.Session()
+    response = session.get(url)
+    soup = BeautifulSoup(response.text, "html5lib")
+    meta_token = soup.find("meta", attrs={"name": "csrf-token"})
+    data = session.post(url,
+                        data=parameter,
+                        headers={"x-csrf-token": meta_token.attrs['content']})
+    data = json.loads(data.text)
+    return data
 
-    # pprint(data['waste_discharge'][str(year)])
-
-    # pprint(data['trash_type_colors'])
-
+def filter_calendar_for_year(data, year):
+    ''' filters the calendar data by year '''
     output = [['Subject', 'Start date', 'All Day Event']]
-
     for month in range(1,13):
         for day in data['waste_discharge'][str(year)][str(month)].keys():
             subject = garbage_cans[data['waste_discharge'][str(year)][str(month)][day][0].lower()]
             all_day_event = True
             start_date = "{:02d}/{:02d}/{:02d}".format(month, int(day), year) 
             output.append([subject, start_date, all_day_event])
+    return output
 
-    with open('egn_{}_calendar.csv'.format(year), 'w', newline='') as csvfile:
+def write_csv(calendar_data, parameter, year):
+    ''' writes a csv that you can import to Google Kalender'''
+    with open(create_filename(parameter, year), 'w', newline='') as csvfile:
         spamwriter = csv.writer(csvfile)
-        spamwriter.writerows(output)
+        spamwriter.writerows(calendar_data)
 
-
-
+def main():
+    ''' loads calendar data from the egn website and
+    creates a csv-file. '''
+    parameter = create_parameter(city, district, street, house_number)
+    data = load_data(EGN_URL, parameter)
+    calendar_data = filter_calendar_for_year(data, year)
+    write_csv(calendar_data, parameter, year)
+   
 if __name__ == "__main__":
     main()
